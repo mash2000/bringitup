a67cfced41145f7c325dbcad50868eb381d8e0dc
Ширшиков Максим <Iammaksim2000shirshikov@gmail.com>
2022-04-22 17:36:24 +0300

    download

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import MiniSlider from "./modules/slider/slider-mini";
 import Difference from "./modules/difference";
 import Form from "./modules/forms";
 import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener('DOMContentLoaded', () => {
 	const player = new VideoPlayer('.showup .play', '.overlay');
@@ -49,4 +50,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	new Form('.form').init();
 
 	new ShowInfo('.plus__content').init();
+	new Download('.download').init();
 });
